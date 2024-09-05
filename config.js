6bf87eefbac32c6dc7f101274fae7f6c2d7578b2
Dollar-ai <binanceheisted3@gmail.com>
2024-09-05 09:39:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_5a46d90ebfd444739b627de69a06a8936').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
